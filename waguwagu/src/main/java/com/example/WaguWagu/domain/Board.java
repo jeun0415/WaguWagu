06177e0ac2326a2e38e9f9eda749fff24c46aaf0
DESKTOP-SCJ83VA\user <j_eun0415@naver.com>
2024-09-25 18:10:11 +0900

    240925 build.gradle에 swagger, model mapper 링크 추가

--- waguwagu/src/main/java/com/example/WaguWagu/domain/Board.java
+++ waguwagu/src/main/java/com/example/WaguWagu/domain/Board.java
@@ -1,4 +1,37 @@
 package com.example.WaguWagu.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long b_idx;     // 게시글ID
+
+    @Column(columnDefinition = "int default 0")
+    private int b_count;    // 조회수
+
+    private String b_title;     // 제목
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime b_reg_date;      // 작성시간
+
+    @LastModifiedDate
+    private LocalDateTime b_mod_date;       // 수정시간
+
+    @OneToOne
+    private Recipe recipe;      // 레시피ID FK
 }
--- waguwagu/src/main/java/com/example/WaguWagu/domain/Comments.java
+++ waguwagu/src/main/java/com/example/WaguWagu/domain/Comments.java
@@ -1,4 +1,41 @@
 package com.example.WaguWagu.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Comments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long c_idx;     // 댓글ID
+
+    @Column(length = 200)
+    private String c_content;       // 내용
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime c_reg_date;      // 작성시간
+
+    @LastModifiedDate
+    private LocalDateTime c_mod_date;       // 수정시간
+
+    @ManyToOne
+    private Board board;        // 게시글ID FK
+
+    @ManyToOne
+    private Member member;      // 회원ID FK
 }
