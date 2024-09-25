c76fa3367a166a71440aa95f4b7b02f07fd06ae9
DESKTOP-NM3GHD2\tmddy <lsy03108@gmail.com>
2024-09-26 00:47:27 +0900

    240925 waguwagu 최종 수정

--- waguwagu/src/main/java/com/example/WaguWagu/repository/RecipeRepository.java
+++ waguwagu/src/main/java/com/example/WaguWagu/repository/RecipeRepository.java
@@ -4,6 +4,5 @@ import com.example.WaguWagu.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 }
